f295559f8a07cd29ab1d59d9b9d2b2158be8f9f8
Alex Blackie <alex@blackie.me>
2022-01-30 11:06:28 -0800

    Add function for parsing nextcloud auth headers
    This will pull the current user out of the request by looking up the app
    password in the `Authorization` header, validating it, and then
    returning the user that owns that password.

--- nxc/auth.go
+++ nxc/auth.go
@@ -1,5 +1,14 @@
 package nxc
 
+import (
+	"encoding/base64"
+	"strings"
+
+	"com.blackieops.nucleus/auth"
+	"com.blackieops.nucleus/data"
+	"github.com/gin-gonic/gin"
+)
+
 type PollResponse struct {
 	Poll     PollEndpoint `json:"poll"`
 	LoginURL string       `json:"login"`
@@ -14,4 +23,19 @@ type PollSuccessResponse struct {
 	Server   string `json:"server"`
 	Username string `json:"loginName"`
 	Password string `json:"appPassword"`
+}
+
+func CurrentUser(c *data.Context, g *gin.Context) (*auth.User, error) {
+	authHeaderValue := string(g.GetHeader("Authorization"))[len("Basic "):]
+	authHeaderValueBytes, err := base64.URLEncoding.DecodeString(authHeaderValue)
+	if err != nil {
+		panic(err)
+	}
+	authHeaderPassword := strings.Split(string(authHeaderValueBytes), ":")[1]
+	appPassword, err := FindNextcloudAppPasswordByPassword(c, authHeaderPassword)
+	if err != nil {
+		return &auth.User{}, err
+	} else {
+		return &appPassword.User, nil
+	}
 }
