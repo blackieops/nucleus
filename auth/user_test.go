ac8a4ad0c78d7c213fb0bfc2f3baa993f7a854c4
Alex Blackie <alex@blackie.me>
2022-02-04 08:56:10 -0800

    Add tests for remaining auth User functions
    Half were tested, half were not. This adds the missing tests.

--- auth/user_test.go
+++ auth/user_test.go
@@ -7,6 +7,35 @@ import (
 	testUtils "com.blackieops.nucleus/internal/testing"
 )
 
+func TestFindAllUsers(t *testing.T) {
+	testUtils.WithData(func(ctx *data.Context) {
+		CreateUser(ctx, &User{Username: "admin", Name: "Admin", EmailAddress: "A@example.com"})
+		CreateUser(ctx, &User{Username: "alice", Name: "Alice", EmailAddress: "B@example.com"})
+		users := FindAllUsers(ctx)
+		if len(users) != 2 {
+			t.Errorf("Found incorrect number of users: %d", len(users))
+		}
+		for _, u := range users {
+			if u.Username != "admin" && u.Username != "alice" {
+				t.Errorf("FindAllUsers found unexpected user: %v", u.Username)
+			}
+		}
+	})
+}
+
+func TestFindUser(t *testing.T) {
+	testUtils.WithData(func(ctx *data.Context) {
+		user, err := CreateUser(ctx, &User{Username: "admin", Name: "Admin", EmailAddress: "A@example.com"})
+		if err != nil {
+			t.Errorf("Failed to set up user: %v", err)
+		}
+		found := FindUser(ctx, int(user.ID))
+		if found.ID != user.ID {
+			t.Errorf("Found incorrect user: %d instead of %d", found.ID, user.ID)
+		}
+	})
+}
+
 func TestFindUserByUsername(t *testing.T) {
 	testUtils.WithData(func(ctx *data.Context) {
 		CreateUser(ctx, &User{Username: "admin", Name: "Admin", EmailAddress: "A@example.com"})
