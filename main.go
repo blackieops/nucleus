e5a5ff70baf2af582e643e474d6ca818bcc87e15
Alex Blackie <alex@blackie.me>
2022-02-16 08:38:41 -0800

    Clean up main, move routes into router
    This tries to simplify what we put in `main()` by splitting the routes
    and Gin stuff into its own file/methods, and by moving the flags into
    "global" vars.
    
    Really `main()` should only be for parsing the flags and handling the
    topmost control flow of the program. With these latest changes it seems
    like we're basically at that point.

--- main.go
+++ main.go
@@ -2,25 +2,23 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"com.blackieops.nucleus/auth"
 	"com.blackieops.nucleus/config"
 	"com.blackieops.nucleus/data"
-	"com.blackieops.nucleus/files"
 	"com.blackieops.nucleus/nxc"
-	"com.blackieops.nucleus/web"
-	"github.com/gin-contrib/sessions"
+	"com.blackieops.nucleus/files"
 	"github.com/gin-contrib/sessions/cookie"
-	"github.com/gin-contrib/static"
-	"github.com/gin-gonic/gin"
+)
+
+var (
+	configPath = flag.String("config", "config.yaml", "Path to configuration file.")
+	wantIndex = flag.Bool("index", false, "Index the user files on-disk instead of running the server.")
+	wantMigrate = flag.Bool("migrate", false, "Run database migrations instead of running the server.")
+	wantSeeds = flag.Bool("seed", false, "Insert test data into the database.")
 )
 
 func main() {
-	configPath := flag.String("config", "config.yaml", "Path to configuration file.")
-	wantIndex := flag.Bool("index", false, "Index the user files on-disk instead of running the server.")
-	wantMigrate := flag.Bool("migrate", false, "Run database migrations instead of running the server.")
-	wantSeeds := flag.Bool("seed", false, "Insert test data into the database.")
 	flag.Parse()
 
 	conf, err := config.LoadConfig(*configPath)
@@ -43,34 +41,19 @@ func main() {
 	}
 
 	fsBackend := &files.FilesystemBackend{StoragePrefix: conf.DataPath}
+
 	if *wantIndex {
 		(&files.Crawler{DBContext: dbContext, Backend: fsBackend}).ReindexAll()
 		return
 	}
 
-	authMiddleware := &auth.AuthMiddleware{DBContext: dbContext, Config: conf}
-	sessionStore := cookie.NewStore([]byte(conf.SessionSecret))
-
-	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
-	r.Use(static.Serve("/static", static.LocalFile("static", false)))
-	r.Use(sessions.Sessions("nucleussession", sessionStore))
-
-	nextcloudRouter := &nxc.NextcloudRouter{
-		DBContext:      dbContext,
+	r := &NucleusRouter{
+		Auth:           &auth.AuthMiddleware{DBContext: dbContext, Config: conf},
 		Config:         conf,
+		DBContext:      dbContext,
+		SessionStore:   cookie.NewStore([]byte(conf.SessionSecret)),
 		StorageBackend: fsBackend,
-		Auth:           authMiddleware,
 	}
-	nextcloudRouter.Mount(r.Group("/nextcloud"))
-
-	webRouter := &web.WebRouter{DBContext: dbContext, Auth: authMiddleware}
-	webRouter.Mount(r.Group("/web"))
-
-	r.GET("/", func(c *gin.Context) {
-		// If you hit the root path, you probably wanted the web app.
-		c.Redirect(302, "/web/")
-	})
-
-	r.Run(fmt.Sprintf(":%d", conf.Port))
+	r.Configure()
+	r.Listen(conf.Port)
 }
---
+++ router.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+
+	"com.blackieops.nucleus/auth"
+	"com.blackieops.nucleus/config"
+	"com.blackieops.nucleus/data"
+	"com.blackieops.nucleus/files"
+	"com.blackieops.nucleus/nxc"
+	"com.blackieops.nucleus/web"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+type NucleusRouter struct {
+	DBContext      *data.Context
+	Config         *config.Config
+	SessionStore   sessions.Store
+	Auth           *auth.AuthMiddleware
+	StorageBackend files.StorageBackend
+	router         *gin.Engine
+}
+
+func (nr *NucleusRouter) Configure() {
+	nr.router = gin.Default()
+	nr.router.LoadHTMLGlob("templates/*")
+	nr.router.Use(static.Serve("/static", static.LocalFile("static", false)))
+	nr.router.Use(sessions.Sessions("nucleussession", nr.SessionStore))
+
+	nextcloudRouter := &nxc.NextcloudRouter{
+		DBContext:      nr.DBContext,
+		Config:         nr.Config,
+		StorageBackend: nr.StorageBackend,
+		Auth:           nr.Auth,
+	}
+	nextcloudRouter.Mount(nr.router.Group("/nextcloud"))
+
+	webRouter := &web.WebRouter{
+		DBContext: nr.DBContext,
+		Auth: nr.Auth,
+	}
+	webRouter.Mount(nr.router.Group("/web"))
+
+	nr.router.GET("/", func(c *gin.Context) {
+		// If you hit the root path, you probably wanted the web app.
+		c.Redirect(302, "/web/")
+	})
+}
+
+func (nr *NucleusRouter) Listen(port int) {
+	nr.router.Run(fmt.Sprintf(":%d", port))
+}
